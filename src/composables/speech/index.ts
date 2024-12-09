5954647b616242601f36738e9f8b03a24c719d15
TalexDreamSoul <2418876761@qq.com>
2024-12-09 10:00:41 +0800

    feat(explore): 增加口语化学习内容与样式优化

---
+++ src/composables/speech/index.ts
@@ -0,0 +1,154 @@
+export const articles = [
+  {
+    title: {
+      text: 'The Woman and the Turtles',
+      translation: '女人与乌龟',
+    },
+    articles: [
+      {
+        paragraph: 'This past Sunday my curiosity got the best of me, so I stopped jogging and walked over to her. As I got closer, I realized that the metal cage was in fact a small trap.',
+        translation: '上个星期天，我的好奇心占了上风，所以我停止慢跑，走向她。当我走近时，我意识到金属笼子实际上是一个小陷阱。',
+      },
+      {
+        paragraph: 'There were three turtles, unharmed, slowly walking around the base of the trap. She had a fourth turtle in her lap that she was carefully scrubbing with a spongy brush.',
+        translation: '有三只乌龟，没有受伤，在陷阱底部缓慢地走动。她的腿上有第四只乌龟，她正在用海绵刷仔细擦洗。',
+      },
+      {
+        paragraph: '"Hello," I said. "I see you here every Sunday morning. If you don\'t mind my nosiness, I\'d love to know what you\'re doing with these turtles."',
+        translation: '“你好，”我说。“我每个星期天早上都在这里看到你。如果你不介意我多管闲事，我很想知道你在和这些海龟做什么。”',
+      },
+      {
+        paragraph: 'She smiled. "I\'m cleaning off their shells," she replied. "Anything on a turtle\'s shell, like algae or scum, reduces the turtle\'s ability to absorb heat and impedes its ability to swim. It can also corrode and weaken the shell over time."',
+        translation: '她微笑着。“我正在清理它们的壳，”她回答道。“海龟壳上的任何东西，如藻类或浮渣，都会降低海龟吸收热量的能力并阻碍其游泳的能力。随着时间的推移，它还会腐蚀和削弱壳。”',
+      },
+      {
+        paragraph: '"Wow! That\'s really nice of you!" I exclaimed.',
+        translation: '“哇！你真好！”我惊呼道。',
+      },
+      {
+        paragraph: 'She went on: "I spend a couple of hours each Sunday morning, relaxing by this lake and helping these little guys out. It\'s my own strange way of making a difference."',
+        translation: '她接着说：“我每个星期天早上都会花几个小时在这个湖边放松，帮助这些小家伙。这是我自己独特的改变方式。”',
+      },
+      {
+        paragraph: '"But don\'t most freshwater turtles live their whole lives with algae and scum hanging from their shells?" I asked.',
+        translation: '“但是大多数淡水龟不是一生都在壳上挂着藻类和浮渣吗？”我问道。',
+      },
+      {
+        paragraph: '"Yep, sadly, they do, " she replied.',
+        translation: '“是的，可悲的是，他们有。”她回答道。',
+      },
+      {
+        paragraph: 'I scratched my head. "Well then, don\'t you think your time could be better spent? I mean, I think your efforts are kind and all, but there are fresh water turtles living in lakes all around the world. And 99% of these turtles don\'t have kind people like you to help them clean off their shells. So, no offense… but how exactly are your localized efforts here truly making a difference?"',
+        translation: '我挠了挠头。“那么，你不认为你的时间可以更好地利用吗？我的意思是，我认为你的努力是善良的，但世界各地的湖泊中都有淡水龟。而这些龟中有99%没有像你这样善良的人来帮助它们清理壳。所以，无意冒犯…但你在这里的本地化努力究竟如何真正产生影响？”',
+      },
+      {
+        paragraph: 'The woman giggled aloud. She then looked down at the turtle in her lap, scrubbed off the last piece of algae from its shell, and said, "Sweetie, if this little guy could talk, he\'d tell you I just made all the difference in the world."',
+        translation: '女人咯咯笑了起来，然后低头看了看腿上的乌龟，擦掉了它壳上的最后一块藻类，说道：“亲爱的，如果这个小家伙会说话，他会告诉你我刚刚改变了世界。”',
+      },
+    ],
+  },
+  {
+    title: {
+      text: 'The Bus Ride',
+      translation: '公交车之旅',
+    },
+    articles: [
+      {
+        paragraph: 'The day started out rotten. She overslept and was late for work. Everything that happened at the office contributed to her nervous frenzy. By the time she reached the bus stop for her homeward trip, her stomach was one big knot.',
+        translation: '这一天开始很糟糕。她睡过头了，上班迟到了。办公室发生的一切都加剧了她的紧张情绪。当她到达回家的公交车站时，她的胃已经有一个大疙瘩了。',
+      },
+      {
+        paragraph: 'As usual, the bus was late and jammed. She had to stand in the aisle. As the lurching vehicle pulled her in all directions, her gloom deepened.',
+        translation: '像往常一样，公交车晚点了，堵车了。她不得不站在过道上。当颠簸的车辆把她拉向各个方向时，她的忧郁加深了。',
+      },
+      {
+        paragraph: 'Then she heard a voice from up front boom, "Beautiful day, isn\'t it?" Because of the crowd, she could not see the man, but she heard him as he continued to comment on the spring scenery, calling attention to each approaching landmark. This church. That park. This cemetery. That firehouse. Soon all the passengers were gazing out the windows. The man\'s enthusiasm was so contagious that she found herself smiling for the first time that day.',
+        translation: '然后她听到前排传来一个声音，“天气很好，不是吗？”因为人群太多，她看不见那个男人，但她听到他继续评论春天的景色，引起人们对每一个接近的地标的注意。这个教堂。那个公园。这个墓地。那个消防站。很快所有的乘客都凝视着窗外。那个男人的热情是如此具有感染力，以至于她发现自己那天第一次微笑。',
+      },
+      {
+        paragraph: 'They reached her stop. Maneuvering toward the door, she got a look at their "guide": an older gentleman with a beard, wearing dark glasses and carrying a thin, white cane.',
+        translation: '他们到达了她的车站。向门口移动，她看到了他们的“向导”：一个留着胡须的老绅士，戴着墨镜，拿着一根细白色的手杖。',
+      },
+    ],
+  },
+  {
+    title: {
+      text: 'Wingsuits',
+      translation: '翼装',
+    },
+    articles: [
+      {
+        paragraph: 'The human body is built to match our physical needs, but as a species we have the drive to push our physical limits even further. Taking inspiration from the majestic flying squirrel, wingsuits are designed to provide extreme sportspeople with a new body shape: one that allows them to maximise their flying potential. The ability to jump from a cliff edge or plane and soar through the sky was once deemed to be a possibility only for winged creatures and fantasy superheroes, but these specially designed outfits give those who are brave enough the chance to join native sky-dwellers.',
+        translation: '人体是为了满足我们的身体需求而建造的，但作为一个物种，我们有动力将我们的身体极限推向更远。从雄伟的飞鼠中汲取灵感，翼装被设计为为极限运动员提供一种新的体型：一种使他们能够最大化飞行潜力的体型。从悬崖边缘或飞机上跳跃并在天空中翱翔的能力曾经被认为只有有翼生物和幻想超级英雄才有可能，但这些特别设计的服装为那些勇敢的人提供了加入本地天空居民的机会。',
+      },
+      {
+        paragraph: 'Wingsuits are designed for BASE – building, antenna, span and earth – jumpers and skydivers. Skydivers leap from helicopters and planes, while BASE jumpers take off from fixed objects, such as cliffs or tall buildings. It was Finnish BASE jumper Jari Kuosma who invented today\'s modern wingsuit as he stood over a cliff top in Italy.',
+        translation: '翼装是为BASE（建筑、天线、跨度和地球）跳伞者和跳伞者设计的。跳伞者从直升机和飞机上跳跃，而BASE跳伞者从固定物体上起飞，如悬崖或高楼。芬兰BASE跳伞者Jari Kuosma在意大利的悬崖顶部发明了今天的现代翼装。',
+      },
+    ],
+  },
+  {
+    title: {
+      text: 'Outdated Skills',
+      translation: '过时的技能',
+    },
+    articles: [
+      {
+        paragraph: 'The skills and habits you\'ve spent years honing can become outdated due to massive technological shifts, not to mention changes in the global market. Online community members opened up about the outdated skills that they\'ve mastered that used to be very useful. Check out their answers below.',
+        translation: '你花费多年磨练的技能和习惯可能会因为大规模的技术变革而变得过时，更不用说全球市场的变化了。在线社区成员谈论了他们掌握的过时技能，这些技能曾经非常有用。请查看下面的答案。',
+      },
+      {
+        paragraph: '#1: I can see the spelling errors in anything written. They just jump out at me like they are highlighted.',
+        translation: '#1: 我能看到任何书写中的拼写错误。它们就像被突出显示一样突然出现在我面前。',
+      },
+      {
+        paragraph: '#2: Map reading and being able to use a paper map to get where I am going without turn based directions.',
+        translation: '#2: 地图阅读和使用纸质地图无需转弯指示即可到达目的地。',
+      },
+      {
+        paragraph: '#3: I used to remember at least 50 phone numbers, friends, family and work.',
+        translation: '#3: 我曾经记得至少50个电话号码、朋友、家人和工作。',
+      },
+      {
+        paragraph: '#4: Being able to drive a manual car. Believe it or not, this skill is disappearing with the younger generations.',
+        translation: '#4: 能够驾驶手动挡汽车。信不信由你，这项技能正在随着年轻一代的消失而消失。',
+      },
+      {
+        paragraph: '#5: I know how to shear, wash, card, and Spin wool. I also know how to dye the fibre using plant dyes. At this point all of my socks, mitts, toques, scarves, and sweaters have been made by me.',
+        translation: '#5: 我知道如何剪、洗、梳理和纺羊毛。我还知道如何使用植物染料染色纤维。到目前为止，我所有的袜子、手套、帽子、围巾和毛衣都是由我制作的。',
+      },
+    ],
+  },
+  {
+    title: {
+      text: 'Overcoming Shyness',
+      translation: '克服害羞',
+    },
+    articles: [
+      {
+        paragraph: 'Shyness isn\'t an illness. In fact, everyone\'s been shy at some time or another. However, if it makes you suffer or limits you in your daily life, you should do something about it. We\'re going to give you some quotes to inspire you to overcome your feelings of shyness.',
+        translation: '害羞不是一种疾病。事实上，每个人都有过害羞的时候。然而，如果它让你在日常生活中受苦或限制你，你应该采取行动。我们将给你一些名言来激励你克服害羞的感觉。',
+      },
+      {
+        paragraph: '1. "Shyness is made up of the desire to please and the fear of not getting it". (Edme-Pierre Beauchene)',
+        translation: '1. “害羞是由取悦的欲望和得不到它的恐惧组成的”。（埃德梅-皮埃尔·博尚）',
+      },
+      {
+        paragraph: '2. "The man who asks a question is a fool for a minute, the man who does not ask is a fool for life". (Confucius)',
+        translation: '2. “问问题的人一分钟是傻瓜，不问的人一辈子是傻瓜”。（孔子）',
+      },
+      {
+        paragraph: '3. "To the timid and hesitating, everything is impossible, because it seems so". (Walter Scott)',
+        translation: '3. 对于胆小和犹豫的人来说，一切都是不可能的，因为看起来是这样的。（沃尔特·斯科特）',
+      },
+      {
+        paragraph: '4. "The way to overcome shyness is to become so wrapped up in something that you forget to be afraid". (Claudia Ladybird Johnson)',
+        translation: '4. 克服害羞的方法是变得如此专注于某件事情，以至于忘记了害怕。（克劳迪娅·瓢虫·约翰逊）',
+      },
+      {
+        paragraph: '5. "Your shyness alone will bring you more emotional and material losses than all of your other negative attributes". (Amit Kalantri)',
+        translation: '5. 你的害羞本身会给你带来比其他所有负面特质更多的情感和物质损失。（阿米特·卡兰特里）',
+      },
+    ],
+  },
+]
--- src/pages/explore/speech.vue
+++ src/pages/explore/speech.vue
@@ -1,47 +1,80 @@
 <script setup lang="ts">
 import { ElMessage } from 'element-plus'
 import { Card, NavBar, Sidebar, SidebarItem } from 'vant'
+import { articles } from '~/composables/speech'
 
 const router = useRouter()
 
 function onClickLeft() {
   router.back()
 }
+
+interface ISpeechData {
+  index: number
+}
+
+const speechData = useLocalStorage<ISpeechData>('speechData', {
+  index: 0,
+})
+const currentData = computed(() => articles?.[speechData.value.index || 0])
+
+const spokenText = ref('')
+const {
+  isSupported,
+  isPlaying,
+  // status,
+  // voiceInfo,
+  // utterance,
+  stop: spokenStop,
+  speak,
+} = useSpeechSynthesis(spokenText)
+
+async function handlePlayParagraph(text: string) {
+  spokenText.value = text
+
+  speak()
+}
 </script>
 
 <template>
-  <div class="SpeechPage">
+  <div :class="{ supported: isSupported }" class="SpeechPage">
     <NavBar title="口语化学习" left-text="返回" left-arrow w-full @click-left="onClickLeft" />
 
     <div class="SpeechPage-Main">
-      <p class="SpeechPage-Main-Article">
-        <el-text>
-          &nbsp;&nbsp;Last night, I was in a hotel room. I was alone. The doorbell rang. I opened the door. It was my
-          brother.
-          What surprised me was that he was not wearing a mask. Suddenly, I felt a sharp pain in my chest.
-          I was shocked. I looked at my brother. He was holding a knife. He said, "I have killed you."
-          I was shocked again. I looked at my brother. He was holding a knife. He said, "I have killed you."
-          It's my brother. A man. A man who killed me. Then, I woke up. It was a dream. I still remember the pain.
-          Now I'm safe.
-        </el-text>
-      </p>
-      <p my-2 class="SpeechPage-Main-Translation">
-        <el-text>
-          &nbsp;&nbsp;昨晚，我在酒店房间里。 我一个人。 门铃响了。 我打开门。 是我的兄弟。
-          令我惊讶的是，他没有戴口罩。 突然，我感到胸部剧烈疼痛。
-          我很震惊。 我看着我的兄弟。 他拿着刀。 他说：“我杀了你。”
-          我再次感到震惊。 我看着我的兄弟。 他拿着刀。 他说：“我杀了你。”
-          是我的兄弟。 一个男人。 一个杀了我的男人。 然后，我醒了。 这是一个梦。 我仍然记得痛苦。
-          现在我很安全。
-        </el-text>
-      </p>
+      <div v-for="(paragraph, ind) in currentData.articles" :key="ind" class="SpeechPage-Main-Paragraph">
+        <p class="SpeechPage-Main-Text">
+          {{ paragraph.paragraph }}
+          <PlayIcon inline @click="handlePlayParagraph(paragraph.paragraph)" />
+        </p>
+        <p class="SpeechPage-Main-Translation">
+          {{ paragraph.translation }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
 .SpeechPage-Main {
+  &-Paragraph {
+    .SpeechPage-Main-Text {
+      color: var(--el-text-color-regular);
+    }
+
+    .SpeechPage-Main-Translation {
+      color: var(--el-text-color-secondary);
+    }
+
+    p {
+      text-indent: 1em;
+    }
+
+    margin: 0 0 1rem;
+  }
+
   padding: 1rem;
+
+  overflow-y: scroll;
 }
 
 .SpeechPage {
