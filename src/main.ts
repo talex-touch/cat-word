d87588416d274f68d02b1e7c2fa0265f2a94cb1b
TalexDreamSoul <2418876761@qq.com>
2025-02-04 11:26:52 +0800

    feat(integrations): 添加 Sentry 用于错误追踪和性能监控
    - 在 .gitignore 中添加 .env.sentry-build-plugin 文件
    - 在 package.json 中添加 @sentry/vite-plugin 和 @sentry/vue 依赖
    - 在 main.ts 中初始化 Sentry
    - 在 vite.config.ts 中添加 sentryVitePlugin

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
+import * as Sentry from '@sentry/vue'
 import { loadSlim } from '@tsparticles/slim'
 import Particles from '@tsparticles/vue3'
 import { InstallCodeMirror } from 'codemirror-editor-vue3'
 import ElementPlus from 'element-plus'
+
 import { createApp } from 'vue'
-
 import { createRouter, createWebHistory } from 'vue-router'
-import { routes } from 'vue-router/auto-routes'
 
+import { routes } from 'vue-router/auto-routes'
 import App from './App.vue'
 import '@unocss/reset/tailwind.css'
-import './styles/main.css'
 
+import './styles/main.css'
 import 'uno.css'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
+
 import './styles/element/index.scss'
 
 import 'vant/lib/index.css'
@@ -23,6 +25,23 @@ const router = createRouter({
   routes,
   history: createWebHistory(import.meta.env.BASE_URL),
 })
+
+Sentry.init({
+  app,
+  dsn: 'https://f3bcf43e8b263241b9f1009f6ce25fdf@o4508024637620224.ingest.us.sentry.io/4508758779101184',
+  integrations: [
+    Sentry.browserTracingIntegration({ router }),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/api\.leavesword\.quotawish\.com(\/.*)?$/], // https://api.leavesword.quotawish.com
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
+
 app.use(ElementPlus)
 app.use(router)
 app.use(InstallCodeMirror)
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 /// <reference types="vitest" />
 
 import path from 'node:path'
@@ -16,53 +17,51 @@ export default defineConfig({
       '~/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-  plugins: [
-    VueMacros({
-      defineOptions: false,
-      defineModels: false,
-      plugins: {
-        vue: Vue({
-          script: {
-            propsDestructure: true,
-            defineModel: true,
-          },
-        }),
-      },
-    }),
 
-    // https://github.com/posva/unplugin-vue-router
-    VueRouter(),
-
-    // https://github.com/antfu/unplugin-auto-import
-    AutoImport({
-      imports: [
-        'vue',
-        '@vueuse/core',
-        VueRouterAutoImports,
-        {
-          // add any other imports you were relying on
-          'vue-router/auto': ['useLink'],
+  plugins: [VueMacros({
+    defineOptions: false,
+    defineModels: false,
+    plugins: {
+      vue: Vue({
+        script: {
+          propsDestructure: true,
+          defineModel: true,
         },
-      ],
-      dts: true,
-      dirs: [
-        './src/composables',
-      ],
-      vueTemplate: true,
-    }),
-
-    // https://github.com/antfu/vite-plugin-components
-    Components({
-      dts: true,
-    }),
-
-    // https://github.com/antfu/unocss
-    // see uno.config.ts for config
-    UnoCSS(),
-  ],
+      }),
+    },
+  }), // https://github.com/posva/unplugin-vue-router
+  VueRouter(), // https://github.com/antfu/unplugin-auto-import
+  AutoImport({
+    imports: [
+      'vue',
+      '@vueuse/core',
+      VueRouterAutoImports,
+      {
+        // add any other imports you were relying on
+        'vue-router/auto': ['useLink'],
+      },
+    ],
+    dts: true,
+    dirs: [
+      './src/composables',
+    ],
+    vueTemplate: true,
+  }), // https://github.com/antfu/vite-plugin-components
+  Components({
+    dts: true,
+  }), // https://github.com/antfu/unocss
+  // see uno.config.ts for config
+  UnoCSS(), sentryVitePlugin({
+    org: 'quotawish',
+    project: 'javascript-vue-vb',
+  })],
 
   // https://github.com/vitest-dev/vitest
   test: {
     environment: 'jsdom',
+  },
+
+  build: {
+    sourcemap: true,
   },
 })
