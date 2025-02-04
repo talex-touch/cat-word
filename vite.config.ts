02edfe20dbea5bbdeb1996a1913173a64ecff2f9
TalexDreamSoul <2418876761@qq.com>
2025-02-04 11:59:40 +0800

    build(vite): 动态加载环境变量
    - 引入 process 模块以获取当前工作目录路径
    - 使用 loadEnv 函数动态加载环境变量
    - 修改 Sentry 插件的 authToken 配置，使用动态加载的环境变量
    - 优化代码结构，使配置更加灵活可扩展

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sentryVitePlugin } from '@sentry/vite-plugin'
 /// <reference types="vitest" />
 
 import path from 'node:path'
+import process from 'node:process'
 import Vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -9,60 +10,64 @@ import Components from 'unplugin-vue-components/vite'
 import VueMacros from 'unplugin-vue-macros/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 import VueRouter from 'unplugin-vue-router/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
-    },
-  },
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd())
 
-  plugins: [VueMacros({
-    defineOptions: false,
-    defineModels: false,
-    plugins: {
-      vue: Vue({
-        script: {
-          propsDestructure: true,
-          defineModel: true,
-        },
-      }),
-    },
-  }), // https://github.com/posva/unplugin-vue-router
-  VueRouter(), // https://github.com/antfu/unplugin-auto-import
-  AutoImport({
-    imports: [
-      'vue',
-      '@vueuse/core',
-      VueRouterAutoImports,
-      {
-        // add any other imports you were relying on
-        'vue-router/auto': ['useLink'],
+  return defineConfig({
+    resolve: {
+      alias: {
+        '~/': `${path.resolve(__dirname, 'src')}/`,
       },
-    ],
-    dts: true,
-    dirs: [
-      './src/composables',
-    ],
-    vueTemplate: true,
-  }), // https://github.com/antfu/vite-plugin-components
-  Components({
-    dts: true,
-  }), // https://github.com/antfu/unocss
-  // see uno.config.ts for config
-  UnoCSS(), sentryVitePlugin({
-    authToken: import.meta.env.VITE_SENTRY_AUTH_TOKEN,
-    org: 'quotawish',
-    project: 'leaves_word',
-  })],
+    },
 
-  // https://github.com/vitest-dev/vitest
-  test: {
-    environment: 'jsdom',
-  },
+    plugins: [VueMacros({
+      defineOptions: false,
+      defineModels: false,
+      plugins: {
+        vue: Vue({
+          script: {
+            propsDestructure: true,
+            defineModel: true,
+          },
+        }),
+      },
+    }), // https://github.com/posva/unplugin-vue-router
+    VueRouter(), // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        '@vueuse/core',
+        VueRouterAutoImports,
+        {
+          // add any other imports you were relying on
+          'vue-router/auto': ['useLink'],
+        },
+      ],
+      dts: true,
+      dirs: [
+        './src/composables',
+      ],
+      vueTemplate: true,
+    }), // https://github.com/antfu/vite-plugin-components
+    Components({
+      dts: true,
+    }), // https://github.com/antfu/unocss
+    // see uno.config.ts for config
+    UnoCSS(), sentryVitePlugin({
+      authToken: env.VITE_SENTRY_AUTH_TOKEN,
+      org: 'quotawish',
+      project: 'leaves_word',
+    })],
 
-  build: {
-    sourcemap: true,
-  },
-})
+    // https://github.com/vitest-dev/vitest
+    test: {
+      environment: 'jsdom',
+    },
+
+    build: {
+      sourcemap: true,
+    },
+  })
+}
